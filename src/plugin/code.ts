a7dbde39331e84a0bd2083bd422b306f1ed4e258
m4ndeokyi <askminjae@gmail.com>
2025-10-11 07:08:40 +0900

    Change plugin UI height from 480 to 600

--- src/plugin/code.ts
+++ src/plugin/code.ts
@@ -3,7 +3,7 @@ import { Model } from "./model";
 import { View } from "./view";
 
 async function main() {
-  figma.showUI(__html__, { visible: true, width: 400, height: 480 });
+  figma.showUI(__html__, { visible: true, width: 400, height: 600 });
 
   console.log("Transportation Map Generator Initialized!");
 
